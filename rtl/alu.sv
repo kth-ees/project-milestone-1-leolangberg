6e5eb76758e30e37fb5b47efa7ec33887e453179
leolangberg <langbergleo@gmail.com>
2025-09-14 10:38:44 +0200

    alu & uncomplete tb

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,17 +1,48 @@
-module alu #(
-  BW = 16 // bitwidth
-  ) (
-  input  logic unsigned [BW-1:0] in_a,
-  input  logic unsigned [BW-1:0] in_b,
-  input  logic             [3:0] opcode,
-  output logic unsigned [BW-1:0] out,
-  output logic             [2:0] flags // {overflow, negative, zero}
-  );
+module alu #( BW = 16 ) (
+	input  logic unsigned [BW-1:0] in_a,
+	input  logic unsigned [BW-1:0] in_b,
+	input  logic             [3:0] opcode,
+	output logic unsigned [BW-1:0] out,
+	output logic             [2:0] flags // {overflow, negative, zero}
+);
+	logic unsigned [BW-1:0] w;
+	logic v, n, z, c; // carry never used...
 
-  // Complete your RTL code here
+	always_comb begin
+		case(opcode)
+			3'b000 : {c, w} = in_a + in_b;		// ADD
+			3'b001 : {c, w} = in_a - in_b;		// SUB
+			3'b010 : w = in_a & in_b;		// AND
+			3'b011 : w = in_a | in_b;		// OR
+			3'b100 : w = in_a ^ in_b;		// XOR
+			3'b101 : {c, w} = in_a + 1;		// INC
+			3'b110 : w = in_a;			// MOVA
+			3'b111 : w = in_b;			// MOVB
+		endcase	
 
+		v = 1'b0;
+		if (f == 3'b000) begin
+			// 2 positive numbers give negative sign or
+			// 2 negative numbers give positive sign
+			if((in_a[BW-1] & in_b[BW-1] & ~w[BW-1]) | 
+			  (~in_a[BW-1] & ~in_b[BW-1] & w[BW-1]))
+				v = 1'b1;
+		end
+		if(f == 3'b001) begin
+			// same as addition but in subtraction in_b is inversed. (A + (-B))
+			if((in_a[BW-1] & ~in_b[BW-1] & ~w[BW-1]) | 
+			  (~in_a[BW-1] & in_b[BW-1] & w[BW-1]))
+				v = 1'b1;
+		end
+		if(f == 3'b101) begin
+			v = in_a[BW-1] ^ in_a[BW-2];
+		end
+	end
+
+	assign n = w[BW-1];
+	assign z = (w == 0) ? 1 : 0;	// ~|w
+	
+	assign out = w;
+	assign flags = {v,n,z};
+	
 endmodule
-
-
-
-
--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -1,28 +1,44 @@
-module alu_tb;
-
-  parameter BW = 16; // bitwidth
-
-  logic unsigned [BW-1:0] in_a;
-  logic unsigned [BW-1:0] in_b;
-  logic             [3:0] opcode;
-  logic unsigned [BW-1:0] out;
-  logic             [2:0] flags; // {overflow, negative, zero}
-
-  // Instantiate the ALU
-  alu #(BW) dut (
-    .in_a(in_a),
-    .in_b(in_b),
-    .opcode(opcode),
-    .out(out),
-    .flags(flags)
-  );
-
-  // Generate stimuli to test the ALU
-  initial begin
-    in_a = '0;
-    in_b = '0;
-    opcode = '0;
-    #10ns;
-    // Complete your testbench code here
-  end
-endmodule
+module alu_tb;                                                                                 
+                                                                                               
+  parameter BW = 16; // bitwidth                                                               
+                                                                                               
+  logic unsigned [BW-1:0] in_a;                                                                
+  logic unsigned [BW-1:0] in_b;                                                                
+  logic             [3:0] opcode;                                                              
+  logic unsigned [BW-1:0] out;                                                                 
+  logic             [2:0] flags; // {overflow, negative, zero}                                 
+                                                                                               
+  // Instantiate the ALU                                                                       
+  alu #(BW) dut (                                                                              
+    .in_a(in_a),                                                                               
+    .in_b(in_b),                                                                               
+    .opcode(opcode),                                                                           
+    .out(out),                                                                                 
+    .flags(flags)                                                                              
+  );                                                                                           
+                                                                                               
+  // Generate stimuli to test the ALU                                                          
+  initial begin                                                                                
+        in_a = '0;                                                                             
+        in_b = '0;                                                                             
+        opcode = '0;                                                                           
+        #10ns;                                                                                 
+                                                                                               
+        // ADDITION CASES                                                                      
+        opcode = 3'b000;                                                                       
+        // 1. POS + POS = POS                                                                  
+        in_a = 1;                                                                              
+        in_b = 2;                                                                              
+        #10ns;                                                                                 
+        // 2. POS + POS -> F:OVERFLOW                                                          
+        in_a = 127;                                                                            
+        in_b = 1;                                                                              
+        #10ns;                                                                                 
+        // 3. POS + NEG -> F:NEGATIVE                                                          
+        in_a = 1;                                                                              
+        in_b = (-2);                                                                           
+        #10ns;                                                                                 
+        // 3. NEG + NEG                                                                        
+        in_a = (-2);                                                                           
+        in_b = (-2);                                                                           
+        #10ns;        
